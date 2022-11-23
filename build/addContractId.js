bf2e7f084ec08569547ea5615e9809ed6e5e172c
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-11-24 00:06:04 +0200

    Deleted comments and logs

--- build/addContractId.js
+++ build/addContractId.js
@@ -4,19 +4,14 @@ exports.addContractId = void 0;
 const variables_1 = require("./variables");
 const utils_1 = require("./utils");
 const addContractId = async () => {
-    const rowsNeedingUpdate = await (0, variables_1.db)('js_messages').select('contract', 'id').where('fk_contract_id', null).limit(6); //whereNull? //limit?
-    // if (rowsNeedingUpdate.length != 0) {
-    //     const contractTable = await db('js_contracts').select('address', 'id');
-    // }
-    console.log('Message rows needing updating', rowsNeedingUpdate);
+    const rowsNeedingUpdate = await (0, variables_1.db)('js_messages').select('contract', 'id').whereNull('fk_contract_id');
     (0, utils_1.v)('Message rows needing updating', rowsNeedingUpdate);
     for (const { id, contract } of rowsNeedingUpdate) {
         try {
-            const contract_id = await (0, variables_1.db)('js_contracts').select('id').where("address", contract);
-            console.log('contract_id', contract_id);
+            const contractIdVec = await (0, variables_1.db)('js_contracts').select('id').where("address", contract);
             await (0, variables_1.db)('js_messages').where('id', id)
                 .update({
-                fk_contract_id: contract_id[0].id,
+                fk_contract_id: contractIdVec[0].id,
             });
         }
         catch (error) {
--- src/addContractId.ts
+++ src/addContractId.ts
@@ -2,19 +2,14 @@ import {db} from "./variables";
 import {v} from "./utils";
 
 export const addContractId = async () => {
-    const rowsNeedingUpdate = await db('js_messages').select('contract', 'id').where('fk_contract_id', null).limit(6); //whereNull? //limit?
-    // if (rowsNeedingUpdate.length != 0) {
-    //     const contractTable = await db('js_contracts').select('address', 'id');
-    // }
-    console.log('Message rows needing updating', rowsNeedingUpdate);
+    const rowsNeedingUpdate = await db('js_messages').select('contract', 'id').whereNull('fk_contract_id');
     v('Message rows needing updating', rowsNeedingUpdate)
     for (const { id, contract } of rowsNeedingUpdate) {
         try {
-            const contract_id = await db('js_contracts').select('id').where("address", contract);
-            console.log('contract_id', contract_id)
+            const contractIdVec = await db('js_contracts').select('id').where("address", contract);
             await db('js_messages').where('id', id)
                 .update({
-                    fk_contract_id: contract_id[0].id,
+                    fk_contract_id: contractIdVec[0].id,
                 })
         } catch (error) {
             console.error('Issue updating fk_contract_id in messages', error)
