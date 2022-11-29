a2d2afafa14da05ba6012e079ecca20e57748520
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-11-30 00:20:11 +0200

    Check properties of agent, task and config

--- src/checkSynced.ts
+++ src/checkSynced.ts
@@ -6,17 +6,49 @@ export const checkSynced = async () => {
     v('Not synced', notSynced)
     for (const { id } of notSynced) {
         try {
-            const inAgents = await db('js_agents').where('fk_cb_id', id).count('fk_cb_id', id);
-            const inTasks = await db('js_tasks').where('fk_cb_id', id).count('fk_cb_id', id);
-            const inConfig = await db('js_config').where('fk_cb_id', id).count('fk_cb_id', id);
-            if (inAgents[0].count != '0' && inTasks[0].count != '0' && inConfig[0].count != '0') {
+            const inAgents = await db('js_agents').where('fk_cb_id', id);
+            const inTasks = await db('js_tasks').where('fk_cb_id', id);
+            const inConfig = await db('js_config').where('fk_cb_id', id);
+            if (!agentNotSynced(inAgents) && !taskNotSynced(inTasks) && !configNotSynced(inConfig)) {
                 await db('js_contract_block_piv').where('id', id)
-                .update({
-                    state_synced: true,
-                })
+                    .update({
+                        state_synced: true,
+                    })
             }
         } catch (error) {
             console.error('Issue updating state_synced in js_contract_block_piv', error)
         }
+    }
+}
+
+// Check if agent hasn't been synced
+const agentNotSynced = (agents) => {
+    if (agents.length == 0)   {
+        return true
+    } else {
+        const agent = agents[0];
+        return (agent.address == null || agent.is_active == null || agent.register_start == null)
+    }
+}
+
+// Check if task hasn't been synced
+const taskNotSynced = (tasks) => {
+    if (tasks.length == 0)   {
+        return true
+    } else {
+        const task = tasks[0];
+        return (task.hash == null || task.owner == null || task.interval_type == null 
+            || task.stop_on_fail == null)
+    }
+}
+
+// Check if config hasn't been synced
+const configNotSynced = (configs) => {
+    if (configs.length == 0)   {
+        return true
+    } else {
+        const config = configs[0];
+        return (config.paused == null || config.owner_id == null || config.min_tasks_per_agent == null 
+            || config.agent_fee == null || config.native_denom == null)
     }
 }
